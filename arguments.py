a95209183138b90fdefc50aa88438629f25397d6
nick88stam <66620748+nick88stam@users.noreply.github.com>
2020-06-08 14:03:13 +0300

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -6,7 +6,7 @@ def get_argument_parser():
 
     parser.add_argument('--population', type=int, default=10, help='Number of individuals in the population')
 
-    parser.add_argument('--iterations', type=int, default=10, help='Number of evolution iterations')
+    parser.add_argument('--iterations', type=int, default=100, help='Number of evolution iterations')
 
     parser.add_argument('--k', type=float, default=0.5, help='Externality parameter')
 
