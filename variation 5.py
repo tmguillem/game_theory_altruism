40aef0afc06d1dd6442fe2009c5f7337af575ccc
nick88stam <stamatopoulosni@gmail.com>
2020-06-26 14:12:52 +0300

    Final changes

--- variation 5.py
+++ variation 5.py
@@ -13,16 +13,16 @@ def main():
     strategies x and the parameters alpha evolve
     """
 
-    population = 200
+    population = 500
     iterations = 5000
     k = 0.5
-    m = 10
+    m = 0.5
     mu = 0.01
-    mutable_params = ['alpha']
+    mutable_params = ['x','alpha']
     x = None
     alpha = None
     reprod_method = "payoff"
-    rational = False
+    rational = True
     reciprocal = True
 
 
@@ -33,20 +33,26 @@ def main():
 
     summary = genetic_algo.run()
     print(np.mean(summary['x'][-1, :]))
+
     ax = plot_param_evolution(summary['x'])
+    xlim = ax.get_xlim()
+    x_tilde = np.linspace(xlim[0], xlim[1], 100)
+    y_tilde = m/(2-k) * np.ones_like(x_tilde)
+    plt.plot(x_tilde, y_tilde, color='tab:blue', path_effects=[pe.Stroke(linewidth=5, foreground='w'), pe.Normal()],
+             lw=2, label='Theoretical value', alpha=0.7)
     ax.set_ylabel('log(x) histogram')
     ax.legend()
-    ax.set_title('Variation 4 experiment - x evolution')
+    ax.set_title('x evolution')
 
     ax = plot_param_evolution(summary['alpha'], logy=False)
     xlim = ax.get_xlim()
-    x_tilde = np.linspace(xlim[0], xlim[1], 100)
-    y_tilde = (2-k)/2 * np.ones_like(x_tilde)
-    plt.plot(x_tilde, y_tilde, color='tab:blue', path_effects=[pe.Stroke(linewidth=5, foreground='w'), pe.Normal()],
-             lw=2, label='Theoretical value', alpha=0.7)
+    #x_tilde = np.linspace(xlim[0], xlim[1], 100)
+    #y_tilde = 1 * np.ones_like(x_tilde)
+    #plt.plot(x_tilde, y_tilde, color='tab:blue', path_effects=[pe.Stroke(linewidth=5, foreground='w'), pe.Normal()],
+           #  lw=2, label='Theoretical value', alpha=0.7)
     ax.set_ylabel('alpha histogram')
     ax.legend()
-    ax.set_title('Variation 4 experiment - alpha evolution')
+    ax.set_title('Alpha evolution')
 
     plt.show()
 
